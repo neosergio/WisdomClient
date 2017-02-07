31b623aa5c3a4a89d3f9309a8de07cf5ba8668a4
Sergio Infante <rsinfante@gmail.com>
2017-02-07 00:41:23 -0500

    Add draft version to parse data and temporally store in an array

---
+++ WisdomClient/Util/Constans.swift
@@ -0,0 +1,13 @@
+//
+//  Constans.swift
+//  WisdomClient
+//
+//  Created by Sergio Infante on 2/6/17.
+//  Copyright © 2017 Sergio Infante. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let mainURL : String = "https://wisdomhappybox.herokuapp.com/api/"
+}
--- WisdomClient/ViewController.swift
+++ WisdomClient/ViewController.swift
@@ -7,12 +7,56 @@
 //
 
 import UIKit
+import Alamofire
+
+struct card {
+    let id: Int!
+    let title: String!
+    let text: String!
+}
 
 class ViewController: UIViewController {
+    
+    var cards = [card]()
+    
+    typealias JSONStandar = [AnyObject]
+    
+    func callAlamo(url: String){
+        Alamofire.request(url).responseJSON(completionHandler: {
+            response in
+            self.parseData(JSONData: response.data!)
+            let cardPicked = self.pickRandomCard(array: self.cards)
+            print(cardPicked)
+        })
+    }
 
+    func parseData(JSONData: Data){
+        do{
+            let readableJSON = try JSONSerialization.jsonObject(with: JSONData, options: .mutableContainers) as! JSONStandar
+            let items = readableJSON
+            for i in 0..<items.count{
+                let item = items[i]
+                let card_id = item["id"] as! Int
+                let title = item["title"] as! String
+                let text = item["text"] as! String
+                cards.append(card.init(id: card_id, title: title, text: text))
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
+    func pickRandomCard(array: Array<Any>) -> AnyObject {
+        let randomNumber = Int(arc4random_uniform(UInt32(array.count)))
+        return array[randomNumber] as AnyObject
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let cardsURL = "\(Constants.mainURL)cards/"
+        print(cardsURL)
+        callAlamo(url: cardsURL)
     }
 
     override func didReceiveMemoryWarning() {
