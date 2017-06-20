3f70954f40c442c1ae2053915f8018697ce15a76
Sergio Infante <raulsergio9@gmail.com>
2017-06-20 15:07:53 -0500

    Add Geeky Wisdom as name

--- WisdomClient/Util/Constans.swift
+++ WisdomClient/Util/Constans.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct Constants {
     static let mainURL : String = "https://wisdomhappybox.herokuapp.com/api/"
-    static let infoText : String = "Swipe left or swipe right to get more and more geek wisdom."
+    static let infoText : String = "Swipe left or swipe right to get more and more geeky wisdom."
     static let copyrightText : String = "© All copyrights belong to their respective owners. Images and text owned by other copyright holders, these images and text are not intended to generate income for the author of this application."
     
 }
